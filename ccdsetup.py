9daf85ca8aa19b9729edad198b06fe76236fee79
Snyder005 <snyder18@stanford.edu>
2016-02-25 09:58:41 -0800

    Monkey patched subprocess.check_output for Python 2.6

--- ccdsetup.py
+++ ccdsetup.py
@@ -15,6 +15,21 @@ import exposure
 from time import sleep
 from Phidgets.Devices.InterfaceKit import InterfaceKit
 
+if "check_output" not in dir( subprocess ): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd)
+        return output
+    subprocess.check_output = f
+
 ###############################################################################
 ##
 ##  Miscellaneous Functions
