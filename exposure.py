693476e87df50b4c6b7f1b1d4d1531775528c579
Snyder005 <snyder18@stanford.edu>
2016-05-17 13:27:58 -0700

    Progress bar implemented, updates to exposure function.

--- exposure.py
+++ exposure.py
@@ -89,28 +89,29 @@ def scan(filebase, *args, **kwargs):
 ##
 ###############################################################################
 
-def im_acq(mode, filebase="test", time=0.00, seqnum=1):
-    """Perform an image exposure of given mode."""
+def im_acq(mode, filebase="test", exptime=0.00, seqnum=1, **kwargs):
 
-    filename = "{0}.{1}.fits".format(filebase, seqnum)
+    # If bias frame, set exptime to 0.00s
+    if mode == "bias":
+        exptime = 0.00
+    
+    filename = "{0}.{1}.{2}s.{3}.fits".format(filebase, mode,
+                                             exptime, seqnum)
 
-    ## If output file already exists raise an error
+    ## Check that file doesn't exist already
     if os.path.isfile(filename):
         raise IOError
 
     ## Do exposure depending on specified mode
-    if mode == "bias":
-        output = subprocess.check_output(["dark_acq", "0.00",
+    if mode == "exp":
+        output = subprocess.check_output(["exp_acq", "{0}".format(exptime),
                                           "{0}".format(filename)])
-    elif mode == "dark":
-        output = subprocess.check_output(["dark_acq", "{0}".format(time),
-                                          "{0}".format(filename)])
-    elif mode == "exp":
-        output = subprocess.check_output(["exp_acq", "{0}".format(time),
+    elif mode in ['bias', 'dark']:
+        output = subprocess.check_output(["exp_acq", "{0}".format(exptime),
                                           "{0}".format(filename)])
 
-    ## Update header
-    update_header(filename, testtype=mode.upper(), imgtype=mode.upper(), exptime=time)
+    ## Update FITs header
+    update_header(filename, mode, exptime, seqnum, **kwargs)
     return filename
 
 def stack(mode, filebase, imcount, time, start=0):
@@ -146,35 +147,46 @@ def series(mode, filebase, mintime, maxtime, step):
 ##
 ###############################################################################
 
-def update_header(filepath, **kwargs):
+def update_header(filepath, mode, exptime, seqnum, **kwargs):
 
-    ## Get header items from kwargs dictionary
-    imagetag = kwargs.pop('imagetag', '[lots of digits]')
+    ## Exposure dependent kwargs
+    filename = filepath
+    if mode in ['bias', 'dark']:
+        testtype = 'dark'
+    elif mode == "exp":
+        testtype = 'obs'
+    imgtype = mode.upper()
+    seqnum = seqnum
+    temp_set = kwargs.pop('temp_set', -95.00) ## Implement in later versions
+    ccdtemp = kwargs.pop('ccdtemp', -95.12) ## Implement in later versions
+    mondiode = kwargs.pop('mondiode', 143.12) ## Implement in later versions
+    exptime = exptime
+
+    ## Filter and monowl are mutually exclusive
+    if 'monowl' in kwargs:
+        monowl = kwargs.get('monowl')
+        filter_name = 'N/A'
+    elif 'filter_name' in kwargs:
+        monowl = 'N/A'
+        filter_name = kwargs.get('filter_name')
+
+    ## Settings from INI file
+    imagetag = kwargs.pop('imagetag', '[lots of digits]') ## Need to change
     tstand = kwargs.pop('tstand', 'Stanford-KGLab')
     instrument = kwargs.pop('instrument', 'SAO16')
     controller = kwargs.pop('controller', 'SAO16')
     contnum = kwargs.pop('contnum', 1)
     ccd_manu = kwargs.pop('ccd_manu', 'ITL')
     ccd_type = kwargs.pop('ccd_type', '3800C')
-    ccd_sern = kwargs.pop('ccd_sern', '19351') 
+    ccd_sern = kwargs.pop('ccd_sern', '19351')
     lsst_num = kwargs.pop('lsst_num', '3800C-033')
-    testtype = kwargs.pop('testtype', 'FLAT')
-    imgtype = kwargs.pop('imgtype', 'FLAT')
-    seqnum = kwargs.pop('seqnum', 123)
-    temp_set = kwargs.pop('temp_set', -95.00)
-    ccdtemp = kwargs.pop('ccdtemp', -95.12)
-    mondiode = kwargs.pop('mondiode', 143.12)
-    monowl = kwargs.pop('monowl', 550.00)
-    filter_name = kwargs.pop('filter_name', '550LP')
-    exptime = kwargs.pop('exptime', 10.00)
-    shut_del = kwargs.pop('shut_del', 100.00)
-    ctrlcfg = kwargs.pop('ctrlcfg', 'abcde.xml')
-    filename = kwargs.pop('filename', os.path.basename(filepath))
+    shut_del = kwargs.pop('shut_del', 0.00)
+    ctrlcfg = kwargs.pop('ctrlcfg', 'abcde.xml') ## Need to look up
     binx = kwargs.pop('binx', 1)
     biny = kwargs.pop('biny', 1)
     headver = kwargs.pop('headver', 1)
-    ccdgain = kwargs.pop('ccdgain', 5.52)
-    ccdnoise = kwargs.pop('ccdnoise', 6.0)
+    ccdgain = kwargs.pop('ccdgain', 5.52) ## Need to change
+    ccdnoise = kwargs.pop('ccdnoise', 6.0) ## Need to change
 
     ## Update fits header
     fits = fitsio.FITS(filepath, 'rw')
@@ -187,17 +199,21 @@ def update_header(filepath, **kwargs):
     fits[0].write_key('CCD_TYPE', ccd_type, comment='CCD Model Number')
     fits[0].write_key('CCD_SERN', ccd_sern, comment="Manufacturers' CCD Serial Number")
     fits[0].write_key('LSST_NUM', lsst_num, comment='LSST Assigned CCD Number')
-    fits[0].write_key('TESTTYPE', testtype, comment='dark:fe55:flat:lambda:spot:sflat_nnn:trap')
+    fits[0].write_key('TESTTYPE', testtype,
+                      comment='dark:fe55:flat:lambda:spot:sflat_nnn:trap')
     fits[0].write_key('IMGTYPE', imgtype, comment='BIAS, DARK, ...')
-    fits[0].write_key('SEQNUM', seqnum, comment='Sequence number extracted from the original filename')
+    fits[0].write_key('SEQNUM', seqnum,
+                      comment='Sequence number extracted from the original filename')
     fits[0].write_key('TEMP_SET', temp_set, comment='Temperature set point')
     fits[0].write_key('CCDTEMP', ccdtemp, comment='Measured temperature')
     fits[0].write_key('MONDIODE', mondiode, comment='Current in the monitoring diode')
     fits[0].write_key('MONOWL', monowl, comment='Monochromator wavelength')
     fits[0].write_key('FILTER', filter_name, comment='Name of the filter')
     fits[0].write_key('EXPTIME', exptime, comment='Exposure Time in Seconds')
-    fits[0].write_key('SHUT_DEL', shut_del, comment='Delay between shutter close command and readout')
-    fits[0].write_key('CTRLCFG', ctrlcfg, comment='Name of the CCD controller configuration file')
+    fits[0].write_key('SHUT_DEL', shut_del,
+                      comment='Delay between shutter close command and readout')
+    fits[0].write_key('CTRLCFG', ctrlcfg,
+                      comment='Name of the CCD controller configuration file')
     fits[0].write_key('FILENAME', filename, comment='Original name of the file')
     fits[0].write_key('BINX', binx, comment='[pixels] binning along X axis')
     fits[0].write_key('BINY', biny, comment='[pixels] binning along Y axis')
